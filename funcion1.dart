09a0a2ed0323f0ba1701294d5517894209ba44ab
Rodolfo Casillas <164534036+Rodolfo-Casillas-1054@users.noreply.github.com>
2025-02-07 16:48:15 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+//Funcion que no devuelve valor 
+//Funcion saludar 
+void saludar(){
+  print("Hola Mundo");
+}
+
+void escribeNombre(){
+  print("Rodolfo Casillas");
+}
+
+void main(){
+  //Rodolfo Casillas: 22308051281054
+  //llama a la funcion saludar
+  print("Rodolfo Casillas: 22308051281054");
+  print('llamando a la funcion saludar');
+  saludar();
+  print('llamando a la funcion escribeNombre');
+  escribeNombre();
+
+}
