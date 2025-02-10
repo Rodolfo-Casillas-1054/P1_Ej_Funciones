54ff34ad13667171b6befc56005c1a1db3014844
Rodolfo Casillas <164534036+Rodolfo-Casillas-1054@users.noreply.github.com>
2025-02-10 18:04:24 +0000

    Ejemplo Funcion Rama

---
+++ funcion2.dart
@@ -0,0 +1,15 @@
+//Funciones creadas por el usuario
+void suma(int a, int b){
+  print("La suma es: ${a + b}");
+}
+
+void resta(int a, int b){
+  print("La resta es: ${a - b}");
+}
+
+void main(){
+  print("Rodolfo Casillas: 22308051281054");
+  print("llamando a la funcion suma y resta");
+  suma(10, 20);
+  resta(20, 10);
+}
